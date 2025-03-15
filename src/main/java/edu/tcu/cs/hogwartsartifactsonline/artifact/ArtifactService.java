41eb9920bb86567cd58d5a853bcafba73b966243
alyssaturenne <aturenne22@shschools.org>
2025-03-15 17:59:14 -0500

    Artifact CRUD done! Close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
@@ -0,0 +1,58 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import edu.tcu.cs.hogwartsartifactsonline.artifact.utils.IdWorker;
+import edu.tcu.cs.hogwartsartifactsonline.artifact.ArtifactNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+public class ArtifactService {
+
+    private final ArtifactRepository artifactRepository;
+
+    private final IdWorker idWorker;
+
+    public ArtifactService(ArtifactRepository artifactRepository, IdWorker idWorker) {
+        this.artifactRepository = artifactRepository;
+        this.idWorker = idWorker;
+    }
+
+    public Artifact findById(String artifactId){
+        return this.artifactRepository.findById(artifactId)
+                .orElseThrow(() -> new ArtifactNotFoundException(artifactId));
+    }
+
+    public List<Artifact> findAll(){
+        return this.artifactRepository.findAll();
+    }
+
+    public Artifact save(Artifact newArtifact){
+        newArtifact.setId(idWorker.nextId() + "");
+        return this.artifactRepository.save(newArtifact);
+    }
+
+    public Artifact update(String artifactId, Artifact update) {
+       return this.artifactRepository.findById(artifactId)
+               .map(oldArtifact -> {
+                   oldArtifact.setName(update.getName());
+                   oldArtifact.setDescription(update.getDescription());
+                   oldArtifact.setImageURL(update.getImageURL());
+                   return this.artifactRepository.save(oldArtifact);
+               })
+               .orElseThrow(() -> new ArtifactNotFoundException(artifactId));
+
+       //save is smart - if artifact is not found, it will create a new one. if it is found, it will perform update
+
+    }
+
+    public void delete(String artifactId){
+        this.artifactRepository.findById(artifactId)
+                .orElseThrow(() -> new ArtifactNotFoundException(artifactId));
+        this.artifactRepository.deleteById(artifactId);
+    }
+
+}
